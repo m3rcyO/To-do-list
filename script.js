2f4ba9baf57dbcfabacf09658819b49d5b7e29fa
M3rcyO <mercyoladeinde7@gmail.com>
2024-10-03 13:50:26 +0100

    implemented 'add task' functionality for enter key

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const input = document.getElementById('inputBox')
-const listItems = document.getElementById('listItems')
+const input = document.getElementById('inputBox');
+const listItems = document.getElementById('listItems');
 
 function addTask() {
-    
+
     if (input.value === '') {
         alert("Please enter a task")
     } else {
 
         let li = document.createElement('li')
-        
+
         const checkbox = document.createElement('input')
         checkbox.type = 'checkbox'
         checkbox.classList.add('checkbox')
-        checkbox.onclick = function(){
+        checkbox.onclick = function () {
             li.classList.toggle('completed')
         }
         li.appendChild(checkbox)
@@ -24,16 +24,20 @@ function addTask() {
         const deleteBtn = document.createElement('button')
         deleteBtn.classList.add('delete-btn')
         deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>'
-        deleteBtn.onclick = function () { 
+        deleteBtn.onclick = function () {
             li.remove()
         }
         li.appendChild(deleteBtn)
 
         listItems.appendChild(li)
 
-       
+
         input.value = '';
     }
 }
 
-        
+input.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
